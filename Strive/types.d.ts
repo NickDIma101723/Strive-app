6f082f2798fea97e1b365e94c991d2c82a75ff87
NickDIma101723 <101723@glr.nl>
2025-06-26 11:39:53 +0000

    issue fixed

---
+++ Strive/types.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="expo/types" />
+
+// Declare modules for better VS Code support
+declare module '@react-navigation/native' {
+  export * from '@react-navigation/native/lib/typescript/src';
+}
+
+declare module 'expo-router' {
+  export * from 'expo-router/build';
+}
